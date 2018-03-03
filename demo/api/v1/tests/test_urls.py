99f5dfacf6a7792d84957169ae8cbe5142707703
cai <jack_czm@vip.sina.com>
2018-03-03 10:58:31 +0800

    add api/v1/echo test

---
+++ demo/api/v1/tests/test_urls.py
@@ -0,0 +1,14 @@
+from django.test import Client, TestCase
+
+
+class TestApiURLs(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_echo_res(self):
+        response = self.client.get('/api/v1/echo?msg=hello')
+        self.assertEqual(response.content, '{"msg":"hello"}')
+
+    def test_echo_404(self):
+        response = self.client.get('/api/v1/echo?hello=hello')
+        self.assertEqual(response.status_code, 404)
